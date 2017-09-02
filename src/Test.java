8d6919edf0b0230ee0bb7a0ac6e856eddd2cc8d3
Indiver Kumar <ink@censhare.com>
2017-09-03 00:27:24 +0530

    MASTER MERGE INDIVER

--- src/Test.java
+++ src/Test.java
@@ -6,5 +6,7 @@ public class Test {
 		System.out.println("LOKESH");
 		System.out.println("MASTER");
 		System.out.println("test3");
+		System.out.println("LOKESH");
+		System.out.println("MASTER");
 	}
 }
